b17649f14c3f449354d8df7c75506556f880529d
chinmaybiswaltech <chinmaybiswaltech@gmail.com>
2022-04-16 21:53:47 +0530

    changes with initial spring-security

--- src/main/java/com/chinmaybiswaltec/resumeportal/ResumePortalApplication.java
+++ src/main/java/com/chinmaybiswaltec/resumeportal/ResumePortalApplication.java
@@ -1,11 +1,11 @@
-package com.chinmaybiswaltech.resumeportal;
+package com.chinmaybiswaltec.resumeportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages="com.chinmaybiswaltech.repositories")
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class ResumePortalApplication {
 
 	public static void main(String[] args) {
